b048fc9a2b62f263263f128ff0551ced361bd7ca
MikasaBili <980415069@qq.com>
2017-03-17 16:45:41 +0800

    50%

--- src/snake/main.js
+++ src/snake/main.js
@@ -1,10 +1,26 @@
 import './snake.less'
+// 蛇的属性
 const _snake = {
-  body: [0, 1, 2, 3, 4],
+  body: [{
+    x: 0,
+    y: 0
+  }, {
+    x: 1,
+    y: 0
+  }, {
+    x: 2,
+    y: 0
+  }, {
+    x: 3,
+    y: 0
+  }, {
+    x: 4,
+    y: 0
+  }],
   bodyColor: '#888888',
   headColor: '#37c700'
 }
-// 需要把背景和蛇分成2个模块 只用刷新蛇不用刷新背景
+// map的属性
 const _map = {
   width: 600,
   height: 600,
@@ -14,19 +30,28 @@ const _map = {
   },
   gridSize: 30
 }
+
+// 按键的属性
+const _game = {
+  new: 'right',
+  isStart: true,
+  order: []
+}
 class Snake {
   constructor () {
-    Object.assign(this, _snake, _map)
-    this.canvas = document.getElementById('snake')
-    this.bu = document.getElementById('bu')
+    Object.assign(this, _snake, _map, _game)
+    this.snake = document.getElementById('snake')
+    this.map = document.getElementById('map')
   }
   init () {
     this.setMap()
     this.setSnake()
+    this.setKeyboard()
+    this.snakeTime()
   }
   // 设置地图
   setMap () {
-    let canvas = this.bu
+    let canvas = this.map
     canvas.setAttribute('width', this.width)
     canvas.setAttribute('height', this.height)
     let ctx = canvas.getContext('2d')
@@ -40,7 +65,7 @@ class Snake {
   }
   // 画蛇
   setSnake () {
-    let canvas = this.canvas
+    let canvas = this.snake
     canvas.setAttribute('width', this.width)
     canvas.setAttribute('height', this.height)
     let ctx = canvas.getContext('2d')
@@ -50,24 +75,85 @@ class Snake {
       } else {
         ctx.fillStyle = this.bodyColor
       }
-      ctx.fillRect(value * this.gridSize, 0, this.gridSize, this.gridSize)
+      ctx.fillRect(value.x * this.gridSize, value.y * this.gridSize, this.gridSize, this.gridSize)
     })
   }
   // 爬行
   snakeGO () {
     this.body = Array.from(this.body, (value) => {
-      return (value = value + 1)
+      let newVal = value
+      newVal.x = value.x + 1
+      newVal.y = value.y + 0
+      return newVal
     })
+  }
+  // 清除蛇
+  clearSnake () {
+    let canvas = this.snake
+    let ctx = canvas.getContext('2d')
+    ctx.clearRect(0, 0, this.width, this.height)
   }
   // 爬行时间
   snakeTime () {
-    setInterval(() => {
-      this.snakeGO()
-      let canvas = this.canvas
-      let ctx = canvas.getContext('2d')
-      ctx.clearRect(0, 0, this.width, this.height)
-      this.setSnake()
-    }, 1000)
+    const time = () => {
+      setTimeout(() => {
+        console.log(this.isStart, 3)
+        if (!this.isStart) {
+          return false
+        }
+        this.snakeGO() // 设置body数组
+        this.clearSnake() // 清除现在的蛇
+        this.setSnake() // 设置新的数组产生的蛇
+        this.snakeTime()
+      }, 1000)
+    }
+    time()
+  }
+  // 绑定键盘
+  setKeyboard () {
+    document.addEventListener('keydown', this.getKeyboard.bind(this))
+  }
+  // 监控键盘操作
+  getKeyboard (e) {
+    switch (e.keyCode) {
+      case 32:
+        this.suspend()
+        break
+      case 38:
+        this.nextOrder('up')
+        break
+      case 39:
+        this.nextOrder('right')
+        break
+      case 40:
+        this.nextOrder('down')
+        break
+      case 37:
+        this.nextOrder('left')
+        break
+    }
+  }
+  // 暂停
+  suspend () {
+    this.isStart = !this.isStart
+  }
+  // 设置键盘下一步
+  nextOrder (next) {
+    debugger
+    if (this.new === 'right' || this.new === 'left') {
+      if (next !== 'right' && next !== 'left') {
+        this.order.push(next)
+        this.new = next
+        console.log(this.order)
+      }
+    } else
+    if (this.new === 'down' || this.new === 'up') {
+      if (next !== 'down' && next !== 'up') {
+        this.order.push(next)
+        this.new = next
+        console.log(this.order)
+      }
+    }
   }
 }
 let init = new Snake()
