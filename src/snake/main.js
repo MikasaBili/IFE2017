cf859a852c128feb3bfd0412375092f5d27151a3
MikasaBili <980415069@qq.com>
2017-03-21 19:17:15 +0800

    70%

--- src/snake/main.js
+++ src/snake/main.js
@@ -1,22 +1,8 @@
 import './snake.less'
 // 蛇的属性
 const _snake = {
-  body: [{
-    x: 0,
-    y: 0
-  }, {
-    x: 1,
-    y: 0
-  }, {
-    x: 2,
-    y: 0
-  }, {
-    x: 3,
-    y: 0
-  }, {
-    x: 4,
-    y: 0
-  }],
+  initSnkae: [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}, {x: 4, y: 0}],
+  body: [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}, {x: 4, y: 0}],
   bodyColor: '#888888',
   headColor: '#37c700'
 }
@@ -28,26 +14,30 @@ const _map = {
     colorO: '#e6e6e6',
     colorT: '#d2d2d2'
   },
-  gridSize: 30
+  gridSize: 30,
+  star: {}
 }
 
 // 按键的属性
 const _game = {
   new: 'right',
   isStart: true,
-  order: []
+  order: [],
+  games: true
 }
 class Snake {
   constructor () {
     Object.assign(this, _snake, _map, _game)
     this.snake = document.getElementById('snake')
     this.map = document.getElementById('map')
+    this.star = document.getElementById('star')
   }
   init () {
     this.setMap()
     this.setSnake()
     this.setKeyboard()
     this.snakeTime()
+    this.getStarGird()
   }
   // 设置地图
   setMap () {
@@ -83,8 +73,7 @@ class Snake {
     let snakeHeader = this.body[this.body.length - 1] // 头
     let snakeH = Object.assign({}, snakeHeader) // 复制头,这个头代替删除的尾,深拷贝
     this.body.pop() // 删除头
-    this.body.shift() // 尾 同时删除 尾
-    console.log(this.body)
+    let snakeW = this.body.shift() // 尾 同时删除 尾
     if (this.order.length) {
       let next = this.order.shift()
       if (next === 'right') {
@@ -113,8 +102,18 @@ class Snake {
         snakeHeader.y += 1
       }
     }
+    // 下面是判断是否撞墙
+    if (snakeHeader.x >= (this.width / this.gridSize) || snakeHeader.x < 0 || snakeHeader.y >= (this.height / this.gridSize) || snakeHeader.y < 0) {
+      this.games = false
+    }
     this.body.push(snakeHeader) // 把新的头添加到最后面
     this.body.splice(this.body.length - 1, 0, snakeH) // 把上一个头的坐标添加到倒数第二个
+    // 下面是判断蛇的头和star 坐标的头重复则表示吃到了星星,尾巴延长一个
+    if (snakeHeader.x === this.star.x && snakeHeader.y === this.star.y) {
+      this.body.unshift(snakeW)
+      this.clearStar(this.star.x, this.star.y)
+      this.getStarGird()
+    }
   }
   // 清除蛇
   clearSnake () {
@@ -126,15 +125,22 @@ class Snake {
   snakeTime () {
     const time = () => {
       setTimeout(() => {
-        console.log(this.isStart, 3)
         if (!this.isStart) {
           return false
         }
         this.snakeGO() // 设置body数组
         this.clearSnake() // 清除现在的蛇
+        if (!this.games) {
+          this.body = this.initSnkae
+          this.games = true
+          this.setMap()
+          this.setSnake()
+          this.suspend()
+          return false
+        }
         this.setSnake() // 设置新的数组产生的蛇
         this.snakeTime()
-      }, 1000)
+      }, 500)
     }
     time()
   }
@@ -165,6 +171,10 @@ class Snake {
   // 暂停
   suspend () {
     this.isStart = !this.isStart
+    console.log(this.isStart)
+    if (this.isStart) {
+      this.snakeTime()
+    }
   }
   // 设置键盘下一步
   nextOrder (next) {
@@ -172,16 +182,46 @@ class Snake {
       if (next !== 'right' && next !== 'left') {
         this.order.push(next)
         this.new = next
-        console.log(this.order)
       }
     } else
     if (this.new === 'down' || this.new === 'up') {
       if (next !== 'down' && next !== 'up') {
         this.order.push(next)
         this.new = next
-        console.log(this.order)
       }
     }
+  }
+  getStarGird () { // 获取坐标避免和身体重复
+    let starX = Math.round(Math.random().toFixed(3) * 20)
+    let starY = Math.round(Math.random().toFixed(3) * 20)
+    let hasStar = []
+    Array.from(this.body, (value) => {
+      if (value.x === starX && value.y === starY) {
+        hasStar.push(0)
+      } else {
+        hasStar.push(1)
+      }
+    })
+    if (hasStar.indexOf(0) === -1) {
+      this.setStar(starX, starY)
+    } else {
+      this.getStarGird()
+    }
+  }
+  setStar (x, y) { // 设置星星
+    this.star['x'] = x // 把坐标放到this中
+    this.star['y'] = y
+    let canvas = this.star
+    canvas.setAttribute('width', this.width)
+    canvas.setAttribute('height', this.height)
+    let ctx = canvas.getContext('2d')
+    ctx.fillStyle = '#ff9900'
+    ctx.fillRect(x * this.gridSize, y * this.gridSize, this.gridSize, this.gridSize)
+  }
+  clearStar (x, y) { // 清除星星
+    let canvas = this.star
+    let ctx = canvas.getContext('2d')
+    ctx.clearRect(x * this.gridSize, y * this.gridSize, this.gridSize, this.gridSize)
   }
 }
 let init = new Snake()
